ec26bf9148fd9e179ca8359b139e43aa1ce7f7b5
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2025-02-04 22:28:14 -0300

    feat: organizando projeto

---
+++ AprendendoUsoDoCache/Controllers/ProdutoController.cs
@@ -0,0 +1,29 @@
+﻿using AprendendoUsoDoCache.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprendendoUsoDoCache.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        public List<ProdutoEntity> ProdutoList { get; set; } = new()
+        {
+                new ProdutoEntity(1, "Geladeira"),
+                new ProdutoEntity(2, "Fogão"),
+                new ProdutoEntity(3, "Cadeira"),
+                new ProdutoEntity(4, "Mesa"),
+        };
+        
+        public ProdutoController() { }
+
+        [HttpGet]
+        public ActionResult<List<ProdutoEntity>> GetProdutos()
+        {
+            return Ok(ProdutoList);
+        }
+
+
+        
+    }
+}
---
+++ AprendendoUsoDoCache/Entity/ProdutoEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace AprendendoUsoDoCache.Entity
+{
+    public class ProdutoEntity
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ProdutoEntity(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
